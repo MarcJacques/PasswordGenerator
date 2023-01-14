88ffa2e49c1d1af1fafd3bd8ef86e361b96febc1
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-14 16:36:29 +0000

    use getline to perform same task use .substr to perform operation

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() 
 {
   string firstName, lastName, password;
-  cout << "What is your first name? ";
-  cin >> firstName;
+  cout << "What is your name? ";
+  getline(cin, firstName);
   cout << "What is your last name? ";
-  cin >> lastName;
+  getline(cin, lastName);
 
-  password = firstName.substr(0,3) + lastName[0];
+  password = firstName.substr(0,3) + lastName.substr(0, 1);
 
   cout << " Your new password is: " << password;
   return 0;
